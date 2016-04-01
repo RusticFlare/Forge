366c9ad83df3ce7e0ae0bedf27ad9f5833e6e014
James Baker <j_d_baker@hotmail.com>
2016-04-01 18:31:30 +0100

    Update duality-slipknot.rb

--- examples/duality-slipknot.rb
+++ examples/duality-slipknot.rb
@@ -1,1 +1,1 @@
-forge :test, "[[co k ~ ~] [[co,k] ~ ~ k] co [co ~ k ~], [[~ e1] [~ e1 ~] [e1 ~] [~ e1] [e1 ~] [~ e1] [~ e1 ~] [e1 ~] [~ e1] [f1 e1] [b1 ~] a1]/3]"
+forge :duality, "[[co k ~ ~] [[co,k] ~ ~ k] co [co ~ k ~], [[~ e1] [~ e1 ~] [e1 ~] [~ e1] [e1 ~] [~ e1] [~ e1 ~] [e1 ~] [~ e1] [f1 e1] [b1 ~] a1]/3]"
